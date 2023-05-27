400e0ec1296baa208abf0f4120b0d8f00f5936d0
MrSahilShaikh <shaikhsahils2000@gmail.com>
2023-05-27 14:07:56 +0530

    network change
    testnet network change to sepolia testnet

--- myWeb3ProjectOne/hardhat.config.js
+++ myWeb3ProjectOne/hardhat.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
-    defaultNetwork: 'goerli',
+    defaultNetwork: 'sepolia',
     networks: {
       hardhat: {},
-      goerli: {
-        url: 'https://rpc.ankr.com/eth_goerli',
+      sepolia: {
+        url: 'https://sepolia.infura.io/v3/',
         accounts: [`0x${process.env.PRIVATE_KEY}`]
       }
     },
@@ -18,3 +18,5 @@ module.exports = {
     },
   },
 };
+
+//deployed address 0xdafb4058Fe2b48Dc0965A3313B7F8173e49eC59d on thirdweb
