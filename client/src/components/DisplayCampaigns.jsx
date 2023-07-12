9bd56414bce5aa293c3a23bee092934670f7b46d
Krutika Jichkar <93243210+krutikajichkar@users.noreply.github.com>
2023-07-12 20:42:42 +0530

    centered loader

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -17,11 +17,11 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
 
       <div className="flex flex-wrap mt-[20px] gap-[26px]">
         {isLoading && (
-          <img loading="lazy" src={loader} alt="loader" className="w-[100px] h-[100px] object-contain" />
+          <img loading="lazy" src={loader} alt="loader" className="w-[100px] h-[100px] object-contain absolute top-[50%] left-[45%] sm:left-[50%] right-[55%] sm:right-[50%] translate-[50%,50%]" />
         )}
 
         {!isLoading && campaigns.length === 0 && (
-          <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
+          <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183] absolute top-[50%] left-[10%] sm:left-[40%] translate-[-50%,-50%]  ">
             You have not created any campigns yet
           </p>
         )}
