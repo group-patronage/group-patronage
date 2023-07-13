72c6afa54e00f9557aa5939888678e0babdfcc21
Krutika Jichkar <93243210+krutikajichkar@users.noreply.github.com>
2023-07-13 23:58:11 +0530

    added search functionality

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-import FundCard from './FundCard';
-import { loader } from '../assets';
+import FundCard from "./FundCard";
+import { loader } from "../assets";
 
-const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
+const DisplayCampaigns = ({ title, isLoading, campaigns ,searchText}) => {
   const navigate = useNavigate();
 
   const handleNavigate = (campaign) => {
-    navigate(`/campaign-details/${campaign.title}`, { state: campaign })
-  }
-  
+    navigate(`/campaign-details/${campaign.title}`, { state: campaign });
+  };
+
   return (
     <div>
-      <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">{title} ({campaigns.length})</h1>
+      <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">
+        {title} ({campaigns.length})
+      </h1>
 
       <div className="flex flex-wrap mt-[20px] gap-[26px]">
         {isLoading && (
-          <img loading="lazy" src={loader} alt="loader" className="w-[100px] h-[100px] object-contain absolute top-[50%] left-[35%] sm:left-[50%] right-[55%] sm:right-[50%] translate-[50%,50%]" />
+          <img
+            loading="lazy"
+            src={loader}
+            alt="loader"
+            className="w-[100px] h-[100px] object-contain absolute top-[50%] left-[35%] sm:left-[50%] right-[55%] sm:right-[50%] translate-[50%,50%]"
+          />
         )}
 
         {!isLoading && campaigns.length === 0 && (
@@ -26,14 +33,24 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
           </p>
         )}
 
-        {!isLoading && campaigns.length > 0 && campaigns.map((campaign, index) => <FundCard 
+        {/* {!isLoading && campaigns.length > 0 && campaigns.map((campaign, index) => <FundCard 
           key={index}
           {...campaign}
           handleClick={() => handleNavigate(campaign)}
-        />)}
+        />)} */}
+
+        {!isLoading &&
+          campaigns.length > 0 &&
+          campaigns.filter((campaign) => campaign.title?.toLowerCase().includes(searchText?.toLowerCase())).map((campaign, index) => (
+            <FundCard
+              key={index}
+              {...campaign}
+              handleClick={() => handleNavigate(campaign)}
+            />
+          ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DisplayCampaigns
+export default DisplayCampaigns;
