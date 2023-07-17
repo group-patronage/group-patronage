ced236c0f1d6d7858c75f648e79465232ad295d3
Krutika Jichkar <93243210+krutikajichkar@users.noreply.github.com>
2023-07-17 19:17:09 +0530

    fixed logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { useStateContext } from '../context';
 import { CustomButton } from './';
-import { logo, menu, search, profile, github } from '../assets';
+import { logo, menu, search, profile, github, homeIcon } from '../assets';
 import { navlinks } from '../constants';
 import { useAuth0 } from "@auth0/auth0-react"
 import Box from '@mui/material/Box';
@@ -40,6 +40,7 @@ const Navbar = ({searchText , setSearchText}) => {
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
+      
       <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
         <input type="text" placeholder="Search for campaigns" className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none" value={searchText} onChange={handleSearch}/>
 
@@ -140,8 +141,8 @@ const Navbar = ({searchText , setSearchText}) => {
 
       {/* Small screen navigation */}
       <div className="sm:hidden flex justify-between items-center relative">
-        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
-          <img loading="lazy" src={logo} alt="user" className="w-[60%] h-[60%] object-contain" />
+        <div className="w-[50px] h-[50px]  flex justify-center items-center cursor-pointer">
+          <img loading="lazy" src={homeIcon} alt="user" className="w-[60%] h-[60%] object-contain" />
         </div>
 
         <img loading="lazy"
