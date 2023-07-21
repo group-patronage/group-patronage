ee73bf0f9af42eb7e112dd0bea244b46d2f70c68
Krutika Jichkar <93243210+krutikajichkar@users.noreply.github.com>
2023-07-21 12:02:26 +0530

    added not found text

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import FundCard from "./FundCard";
 import { loader } from "../assets";
 
-const DisplayCampaigns = ({ title, isLoading, campaigns ,searchText}) => {
+const DisplayCampaigns = ({ title, isLoading, campaigns, searchText }) => {
   const navigate = useNavigate();
+  const [searchedCampaigns, setSearchedCampaigns] = useState([]);
 
   const handleNavigate = (campaign) => {
     navigate(`/campaign-details/${campaign.title}`, { state: campaign });
   };
+
+  const fetchSearch = () => {
+    setSearchedCampaigns(
+      campaigns.filter((campaign) =>
+        campaign.title?.toLowerCase().includes(searchText?.toLowerCase())
+      )
+    );
+  };
+
+  useEffect(() => {
+    fetchSearch();
+  }, [searchText]);
 
   return (
     <div>
@@ -28,35 +41,33 @@ const DisplayCampaigns = ({ title, isLoading, campaigns ,searchText}) => {
         )}
 
         {!isLoading && campaigns.length === 0 && (
-         <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183] absolute top-[50%] left-[10%] sm:left-[40%] translate-[-50%,-50%]  ">
+          <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183] absolute top-[50%] left-[10%] sm:left-[40%] translate-[-50%,-50%]  ">
             You have not created any campigns yet
           </p>
         )}
 
-<div className="flex flex-wrap gap-6 justify-center mt-4">
-        {!isLoading && campaigns.length > 0 && searchText
-          ? campaigns
-              .filter((campaign) =>
-                campaign.title
-                  ?.toLowerCase()
-                  .includes(searchText?.toLowerCase())
-              )
-              .map((campaign, index) => (
+        {!isLoading && searchedCampaigns?.length === 0 && (
+          <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183] absolute top-[50%] left-[23%] sm:left-[40%] translate-[-50%,-50%]  ">
+            Not Found Any Campaigns !!
+          </p>
+        )}
+
+        <div className="flex flex-wrap gap-6 justify-center mt-4">
+          {!isLoading && campaigns.length > 0 && searchText
+            ? searchedCampaigns.map((campaign, index) => (
                 <FundCard
                   key={index}
                   {...campaign}
                   handleClick={() => handleNavigate(campaign)}
                 />
               ))
-          : campaigns.map((campaign, index) => (
-             
-               <FundCard
-                key={index}
-                {...campaign}
-                handleClick={() => handleNavigate(campaign)}
-              />
-            
-            ))}
+            : campaigns.map((campaign, index) => (
+                <FundCard
+                  key={index}
+                  {...campaign}
+                  handleClick={() => handleNavigate(campaign)}
+                />
+              ))}
         </div>
       </div>
     </div>
